7621dcf74fb402505d8a2d829ef6050386d69536
christopherchinia <chinia214@gmail.com>
2022-06-08 22:35:15 -0400

    commit tree 32dd78ad06d697f58157aa6a7533c5954523d9dd

---
+++ index.js
@@ -0,0 +1,12 @@
+function divide(){
+    return 2000/100;
+}
+
+const square =(num1)=> num1 * num1
+
+const add = (a , b) =>{
+    console.log(`Adding ${a}`);
+    console.log(`adding ${b}`);
+    return a + b;
+}
+sum(a ,b);
